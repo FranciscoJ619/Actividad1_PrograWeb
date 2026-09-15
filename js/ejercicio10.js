e9ed18ead614ee3f75cb303e142777a65afc2b05
FranciscoJ619 <gonzalezsantiagof72@gmail.com>
2026-09-15 01:10:04 -0600

    Add files via upload

---
+++ js/ejercicio10.js
@@ -0,0 +1,11 @@
+function convertir(){
+    var celsius = document.getElementById("gradosC").value;
+    if (celsius===" "){
+        alert("El campo no puede estar vacio");
+    } else if (isNaN(parseInt(celsius))){
+        alert("Debes de ingresar un numero");
+    } else {
+        var resultado=(parseInt(celsius)*9/5)+32;
+        document.getElementById("resultadoF").innerHTML=resultado
+    }
+}
